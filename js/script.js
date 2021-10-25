f96d68afff0e07f5b9f963e40165b3ea12783267
StanislavNikov <stanislavmnikov@gmail.com>
2021-10-26 00:55:19 +0200

    added a loop and a condition to sort height

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-let pokemonList [
-    {name: 'Bulbasaur', heigh: 0.7, type: ['grass', 'poison']},
-    {name: 'Ivysaur', heigh: 1, type: ['grass', 'poison']},
-    {name: 'Venusaur', heigh: 2, type: ['grass', 'poison']}
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, type: ['grass', 'poison']}
 ]
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 2) {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`)
+    } else
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - "Wow! That's BIG!"</p>`)
+}
 console.log(pokemonList)
