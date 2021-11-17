1b0124d32c9dcb2cee5ef7fb483b875728675b68
StanislavNikov <stanislavmnikov@gmail.com>
2021-11-17 20:38:20 +0100

    for loop turned to forEach method - pokemonList

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ let pokemonList = [
     {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
     {name: 'Venusaur', height: 2, type: ['grass', 'poison']}
 ]
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 2) {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})<p></p>`)
+
+// Looping through the pokemonList array
+/* for (let i = 0; i < pokemonList.length; i++) */
+pokemonList.forEach(function(pokemon){
+    if (pokemon.height < 2) {
+        document.write(`${pokemon.name} (height: ${pokemon.height})<p></p>`)
     } else
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - "Wow! That's BIG!"<p></p>`)
-}
+    document.write(`${pokemon.name} (height: ${pokemon.height}) - "Wow! That's BIG!"<p></p>`)
+})
 console.log(pokemonList)
