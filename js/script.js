3849e8a50db0a0f3b871a500931a6e2ce6beea42
StanislavNikov <stanislavmnikov@gmail.com>
2021-11-24 22:25:53 +0100

    pokemon buttons created/ event listener added for each button

--- js/script.js
+++ js/script.js
@@ -3,29 +3,45 @@ let pokemonRepository = (function() {
         {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
         {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
         {name: 'Venusaur', height: 2, type: ['grass', 'poison']}
-    ]    
-        // function add(pokemon) { 
-        //     return (typeof pokemon === 'object') ? (Object.keys(pokemon) === Object.keys(pokemonList) ? pokemonList.push(pokemon): 
-        //     document.write("Please, enter valid values!")) :
-        //     document.write("Please, enter valid values!") 
-        // }          
+    ]      
         function add(pokemon) {
-            return (typeof pokemon === 'object') ? 
-            pokemonList.push(pokemon) : alert("Please, enter valid values!");
-        }
+            return (typeof pokemon === 'object' &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "type" in pokemon) ? 
+            pokemonList.push(pokemon) : 
+            alert("Please, enter valid values!");
+        };
         function getAll() {
             return pokemonList;
+        };
+        function showDetails(pokemon) {
+            console.log(pokemon)
         }
+        function addListItem(pokemon) {
+            let pokelist = document.querySelector('ul');
+            let pokelistItem = document.createElement('li');
+            let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('btn');
+            pokelistItem.appendChild(button);
+            pokelist.appendChild(pokelistItem);
+            button.addEventListener('click', function () {
+                showDetails(pokemon)
+            })
+        };
         return {
             add: add,
-            getAll: getAll
+            getAll: getAll,
+            addListItem: addListItem
         };
 })()
+
+console.log(pokemonRepository.getAll());
+// Adding a pokemon to the pokemonList array
+pokemonRepository.add({name: 'Pikachu', height: 0.3, type: ['electric']});
 // Looping through the pokemonList array
-/* for (let i = 0; i < pokemonList.length; i++) */
-pokemonRepository.getAll().forEach(function(pokemon){
-    if (pokemon.height < 2) {
-        document.write(`${pokemon.name} (height: ${pokemon.height})<p></p>`)
-    } else
-    document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - "Wow! That's BIG!"</p>`)
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
     })
+    
