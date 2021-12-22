e02b8f169db2e48fb48295e315bfac593b0b492a
StanislavNikov <stanislavmnikov@gmail.com>
2021-12-22 14:44:11 +0100

    resolving merging conflict/ README.md updated

---
+++ README.md
@@ -0,0 +1,10 @@
+**Project description:** Explore some pokemons!
+
+**How to get the project running:** Click on a pokeball and check out the attributes of your favourite pokemons.
+
+**Project dependencies:** **_Bootstrap 4_**, **_jQuery_**, **_fetch.umd.js_**, **_promise-polyfill.js_**
+
+**API used:** ***https://pokeapi.co/api/v2/pokemon/?limit=150***
+
+**View project:**
+You can visit the project at ***https://stanislavnikov.github.io/Pokedex/***
--- js/script.js
+++ js/script.js
@@ -19,11 +19,7 @@ let pokemonRepository = (function () {
 
   // Function adds each new pokemon as an item with a button to the list
   function addListItem(pokemon) {
-<<<<<<< HEAD
-    let pokelist = document.getElementById('pokeList'),
-=======
     let pokelist = document.querySelector('#pokeList'),
->>>>>>> branch_b
       pokelistItem = document.createElement('li'),
       button = document.createElement('button');
 
@@ -34,19 +30,6 @@ let pokemonRepository = (function () {
       'col-lg-3',
       'my-2'
     );
-<<<<<<< HEAD
-
-    button.classList.add('pokeButton', 'btn', 'text-light', 'text-capitalize');
-    button.innerText = pokemon.name;
-
-    pokelistItem.appendChild(button);
-    pokelist.appendChild(pokelistItem);
-    button.addEventListener('click', function () {
-      showDetails(pokemon);
-    });
-  }
-
-=======
     button.innerText = pokemon.name;
     button.classList.add(
       'pokeButton',
@@ -63,7 +46,6 @@ let pokemonRepository = (function () {
     });
   }
 
->>>>>>> branch_b
   // Function loads pokemons from the API and adds them to the Array
   function loadList() {
     return fetch(apiUrl)
